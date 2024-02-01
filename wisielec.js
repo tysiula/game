1faabc34c53d57c66c92c8a5e12c57fe38b3d803
tysiula <136995321+tysiula@users.noreply.github.com>
2024-02-01 16:01:24 +0100

    game correct

--- wisielec.js
+++ wisielec.js
@@ -1,106 +1,121 @@
-for(let i=0; i<=8;i++){
-    let life = document.createElement('div');
-    document.getElementById('life').appendChild(life).setAttribute('class', 'lif num '+i);
-}
-
-let letters = ["A", "Ą", "B", "C", "Ć", "D", "E", "Ę", "F", "G", "H", "I", "J", "K", "L", "Ł", "M", "N", "Ń", "O", "Ó", "P", "Q", "R", "S", "Ś", "T", "U", "V", "W", "X", "Y", "Z", "Ż", "Ź"];
-
-window.onload= consoleContent();
-function consoleContent(){
-    let content = '';
-   // cos ='';
-    for(let i=0; i<letters.length; i++){
-        content = content + '<div class="letters" onclick="check('+i+')" id="num'+i+'">'+letters[i]+'</div>';
-        if ((i+1) % 7 ===0)content = content+ '<div style="clear:both;"></div>';
-    }
-    document.getElementById('console').innerHTML= content;
-    //enigmaStart()
-}
-
-let randomNumber = Math.floor(Math.random()*10);
-function setCategory(){
-    if(randomNumber<=5){document.getElementById('category').innerHTML='KRAJE';}
-    else {
-        document.getElementById('category').innerHTML='PRZYSŁOWIA';
-    }
-}
-setCategory()
-
+const USER_LIFE_NUMBER = 9;
+const CATEGORY_BUCKET = 10;
+let letters = ['A', 'Ą', 'B', 'C', 'Ć', 'D', 'E', 'Ę', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'Ł', 'M', 'N', 'Ń', 'O', 'Ó', 'P', 'Q', 'R', 'S', 'Ś', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'Ż', 'Ź'];
 let enigma1 = ['Polska', 'Ukraina', 'Wielka Brytania'];
 let enigma2 = ['Widziały gały co brały', 'Bez pracy nie ma kołaczy'];
-let math1 = Math.round(Math.random() * enigma1.length);
-let math2 = Math.round(Math.random() * enigma2.length);
-let passwordDraw ='';
+let randomNumber;
+let password = '';
+let password2 = '';
 
-function setEnigma(){
-    if(randomNumber<=5){
-        passwordDraw = enigma1[math1];
-    }else {passwordDraw = enigma2[math2]}
-}
-setEnigma()
 
-let password = passwordDraw;
-password = password.toUpperCase();
 let wrongLetter = 0;
-let passwordLength = password.length;
+window.onload = consoleContent();
+drawHearts();
+setCategory()
+setEnigma()
+setPassword2();
 
-
-let password2 = "";
-for(let i=0; i<passwordLength; i++){
-    if(password.charAt(i) === " ") {
-        password2 = password2 + " ";
-    }else {
-        password2 = password2 + "-";
-    }
-}
-function enigmaStart(){
-      document.getElementById('password').innerHTML= password2;
-}
 enigmaStart()
 
+function setCategory() {
+    randomNumber = Math.floor(Math.random() * CATEGORY_BUCKET);
+    let categoryDiv = document.getElementById('category');
+    if (randomNumber <= CATEGORY_BUCKET / 2) {
+        categoryDiv.innerHTML = 'KRAJE';
+    } else {
+        categoryDiv.innerHTML = 'PRZYSŁOWIA';
+     }
+ }
 
 
-String.prototype.markPosition = function (position, mark){
-    if(position>this.length -1){
+function setEnigma() {
+    let math1 = Math.round(Math.random() * (enigma1.length - 1));
+    let math2 = Math.round(Math.random() * (enigma2.length - 1));
+    let passwordDraw = '';
+    if (randomNumber <= CATEGORY_BUCKET / 2) {
+         passwordDraw = enigma1[math1];
+    } else {
+        passwordDraw = enigma2[math2]
+    }
+    password = passwordDraw.toUpperCase();
+ }
+
+
+String.prototype.markPosition = function (position, mark) {
+    if (position > this.length - 1) {
         return this.toString();
-    }else {
-        return this.substr(0, position) + mark + this.substr(position + 1);
-    }enigmaStart()
+    } else {
+        return this.substring(0, position) + mark + this.substring(position + 1);
+    }
 };
 
-function check(number){
-    let elementButton = 'num' + number;
-    let buttonStyle = document.getElementById(elementButton).style;
-    let correct = false;
-    for (let i=0; i<passwordLength; i++){
-        if(password.charAt(i)=== letters[number]){
-            password2 = password2.markPosition(i, letters[number]);
-            correct = true;
+
+function setPassword2() {
+    for (let i = 0; i < password.length; i++) {
+        if (password.charAt(i) === " ") {
+            password2 = password2 + " ";
+        } else {
+            password2 = password2 + "-";
+        }
+     }
+ }
+
+
+function drawHearts() {
+    for (let i = 0; i <= 8; i++) {
+        let life = document.createElement('div');
+        document.getElementById('life').appendChild(life).setAttribute('class', 'lif num ' + i);
+    }
+}
+
+function enigmaStart() {
+    document.getElementById('password').innerHTML = password2;
+ }
+
+
+function consoleContent() {
+    let content = '';
+    for (let i = 0; i < letters.length; i++) {
+        content = content + '<div class="letters" onclick="check(' + i + ')" id="num' + i + '">' + letters[i] + '</div>';
+        if ((i + 1) % 7 === 0) {
+            content = content + '<div style="clear:both;"></div>';
         }
     }
-    if(correct===true){
-        buttonStyle.background = "#003300";
-        buttonStyle.color = "green";
-        buttonStyle.border = "2px solid green";
-        buttonStyle.cursor = "deafult";
-        enigmaStart()
-    }
-    else {
-        buttonStyle.background="#330000";
-        buttonStyle.color= "red";
-        buttonStyle.border= "2px solid red";
-        buttonStyle.cursor= "deafult";
-        document.getElementById(elementButton).setAttribute('onclick', '');
+    setConsoleContent(content);
+}
 
-        wrongLetter++;
-        document.getElementsByClassName("lif num ")[9- wrongLetter].classList.add('ded');
+function setConsoleContent(content) {
+    document.getElementById('console').innerHTML = content;
+}
+
+function check(number) {
+    let elementButton = document.getElementById('num' + number);
+     let correct = false;
+    for (let i = 0; i < password.length; i++) {
+        if (password.charAt(i) === letters[number]) {
+             password2 = password2.markPosition(i, letters[number]);
+             correct = true;
+         }
+     }
+    if (correct === true) {
+        elementButton.classList.add("correctAnswer");
+         enigmaStart()
+    } else {
+        elementButton.classList.add("wrongAnswer");
+        elementButton.removeAttribute('onclick');
+
+         wrongLetter++;
+        document.getElementsByClassName("lif num")[USER_LIFE_NUMBER - wrongLetter].classList.add('ded');
+     }
+    if (password === password2) {
+        setConsoleContent(generateSpan('green', 'WYGRANA'));
     }
-    if(password === password2){
-        document.getElementById('console').innerHTML='<p style="color: green;">WYGRANA</p>' +
-        '<br/> <span class="reset"  onclick="location.reload()" >JESZCZE RAZ?</span>';
-    }
-    if(wrongLetter >=9){
-        document.getElementById('console').innerHTML='<p style="color: red;">PRZEGRANA</p>' +
-        '<br/> <span class="reset" onclick="location.reload()" >JESZCZE RAZ?</span>';
-    }
+    if (wrongLetter >= USER_LIFE_NUMBER) {
+        setConsoleContent(generateSpan('red', 'PRZEGRANA'))
+     }
+}
+
+function generateSpan(color, title) {
+    return `<p style="color: ${color};">${title}</p><br/>
+            <span class="reset" onclick="location.reload()" >JESZCZE RAZ?</span>`
 }
